904564b8089ebc6db34359c95f88421bc2e21e93
tone4ka <toniamik@gmail.com>
2020-10-17 15:14:16 +0300

    bla-bla

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
+  let result;
+  if (Array.isArray(matrix)){
+    result =matrix.map((item,index)=> index%2? item.sort((a,b)=>b-a):item.sort((a,b)=>a-b));
+    result = matrix.flat(Infinity);
+    return result;
+  } 
+
   return [];
+
+ 
 }
